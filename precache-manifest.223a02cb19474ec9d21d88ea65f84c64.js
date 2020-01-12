b78117373648fa5853360f8306a410c1cf7b7a60
Dan Motzenbecker <dan@oxism.com>
2020-01-12 12:23:26 -0500

    update precache manifest

---
+++ precache-manifest.223a02cb19474ec9d21d88ea65f84c64.js
@@ -0,0 +1,32 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b63adf1f6e629c53c49e835510ad8b",
+    "url": "./about.svg"
+  },
+  {
+    "revision": "816c92bf9c26dd87ed21b8ec8ea2ad68",
+    "url": "./favicon.png"
+  },
+  {
+    "revision": "e0ebed3aa665504195c9bf213233634b",
+    "url": "./focus.svg"
+  },
+  {
+    "revision": "abc04d9c03340af3feca2b4e365954a8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "faf24cec671a4c15b1fa429e01e1b6dc",
+    "url": "./logo.svg"
+  },
+  {
+    "url": "./main-7bca98b7be94d0e57d77.css"
+  },
+  {
+    "url": "./main-7bca98b7be94d0e57d77.js"
+  },
+  {
+    "revision": "4cf4c303a93072e15ba6b828b1646b7c",
+    "url": "./random.svg"
+  }
+]);
